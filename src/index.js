0b741e4364194a8640ccfde7c13c09af08af3b49
Franklyn Rocha <faranresolve@gmail.com>
2022-08-16 16:35:44 -0300

    aula 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
 
-import App from './App';
+import App from "./App";
+import store from "./store";
 
 render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root')
+  <ReduxProvider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ReduxProvider>,
+  document.getElementById("root")
 );
